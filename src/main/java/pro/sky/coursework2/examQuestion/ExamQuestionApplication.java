d6db3bf2f205c9d8ff3759598b91291ab9079c75
Yari-i <tipo47181@gmail.com>
2024-12-09 19:02:42 +0500

    Coursework start

---
+++ src/main/java/pro/sky/coursework2/examQuestion/ExamQuestionApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.coursework2.examQuestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamQuestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamQuestionApplication.class, args);
+	}
+
+}
